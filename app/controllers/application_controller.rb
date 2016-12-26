f587b0249b9b7e28bbaf281a872d74f2fdfa8eeb
ngoctran2512 <trandthuyngoc@gmail.com>
2016-12-26 11:09:44 +0700

    sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ApplicationHelper
+  include SessionsHelper
   def render_404
     render file: "public/404.html", layout: false, status: 404
    end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,35 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :suggests, dependent: :destroy
+ attr_accessor :remember_token
+
+   class << self
+     def digest string
+       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+         BCrypt::Engine.cost
+       BCrypt::Password.create string, cost: cost
+     end
+
+     def new_token
+       SecureRandom.urlsafe_base64
+     end
+   end
+
+   def remember
+     self.remember_token = User.new_token
+     update_attributes remember_digest: User.digest(remember_token)
+   end
+
+   def authenticated?(remember_token)
+     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+   end
+
+   def forget
+     update_attributes remember_digest: nil
+   end
+
+   private
+   def downcase_email
+     self.email = email.downcase
+   end
 end
